9d488aafb02f7385e1affa981d7b3246505ee534
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-22 14:49:12 +0100

    composants register et login

---
+++ src/pages/Connexion/pages/Login/Login.js
@@ -0,0 +1,14 @@
+// import style from './Login.module.scss';
+import Formulaire from '../../components/Formulaire/Formulaire';
+
+function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+
+      <Formulaire />
+    </>
+  );
+}
+
+export default Login;
---
+++ src/pages/Connexion/pages/Register/Register.js
@@ -0,0 +1,14 @@
+// import style from './Register.module.scss';
+import Formulaire from '../../components/Formulaire/Formulaire';
+
+function Register() {
+  return (
+    <>
+      <h1>Register</h1>
+
+      <Formulaire />
+    </>
+  );
+}
+
+export default Register;
