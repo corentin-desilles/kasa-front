dda577599c36f190bb135547616175f6493eca87
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-23 09:57:36 +0100

    composant liste logements page admin

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,14 +1,13 @@
+import style from './Admin.module.scss';
 import AddLogement from './components/AddLogement';
 import LogementList from './components/LogementList';
 
 function Admin() {
   return (
-    <div className="d-flex flex-row justify-content-center align-items-center p-20">
-      <div className="card p-20">
-        <h1 className="mb-20">Admin</h1>
-        <AddLogement />
-        <LogementList />
-      </div>
+    <div className={`card b2 ${style.adminContainer} `}>
+      <h1 className="mb-20">Admin</h1>
+      <AddLogement />
+      <LogementList />
     </div>
   );
 }
