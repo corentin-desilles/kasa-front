2ea6b13295ff43073458994ef01c1238fa8398c1
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-23 09:57:58 +0100

    composant item logement pour la liste

--- src/pages/Admin/components/ItemLogement.js
+++ src/pages/Admin/components/ItemLogement.js
@@ -1,5 +1,19 @@
-function ItemLogement() {
-  return <h2>ItemLogement</h2>;
+import style from './ItemLogement.module.scss';
+
+function ItemLogement({ logement: { cover, title } }) {
+  return (
+    <div className={`${style.itemContainer}`}>
+      <span className={`${style.hiddentxt}`}>
+        <a href={cover}>
+          <div className={`${style.title}`}>{title}</div>
+        </a>
+      </span>
+      <span className={`${style.hiddenimg}`}>
+        <img src={cover} alt="apart" />
+      </span>
+      <button className="btn btn-primary">Edit</button>
+    </div>
+  );
 }
 
 export default ItemLogement;
