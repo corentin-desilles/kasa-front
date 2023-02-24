7c0d996152d13106d24f1c28378d97ecbceac386
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-24 10:08:12 +0100

    refactorisation pages connexion

--- src/pages/Connexion/pages/Login/Login.js
+++ src/pages/Connexion/pages/Login/Login.js
@@ -1,12 +1,9 @@
 // import style from './Login.module.scss';
-import Formulaire from '../../components/Formulaire/Formulaire';
 
 function Login() {
   return (
     <>
       <h1>Login</h1>
-
-      <Formulaire />
     </>
   );
 }
