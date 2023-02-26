0093f02ab0bb760f7dfc982545356495708af1ed
Corentin Desilles <corentindesilles974@gmail.com>
2023-02-26 11:46:13 +0100

    composant logement liste page admin

--- src/pages/Admin/AdminLogements/LogementsListe/LogementsListe.js
+++ src/pages/Admin/AdminLogements/LogementsListe/LogementsListe.js
@@ -1,5 +1,27 @@
-function LogementsListe() {
-  return <h3>LogementsListe</h3>;
-}
+import { useContext } from 'react';
+import { useFetchLogements } from '../../../../hooks';
+import Loading from '../../../../components/Loading/Loading';
+import { ApiContext } from '../../../../context/ApiContext';
+import style from './LogementsListe.module.scss';
 
+function LogementsListe() {
+  const BASE_URL_API = useContext(ApiContext);
+  const [logements, isLoading] = useFetchLogements(BASE_URL_API);
+
+  return (
+    <div>
+      {isLoading && !logements.length ? (
+        <div>
+          <Loading />
+        </div>
+      ) : (
+        <div className={`${style.list}`}>
+          {logements.map(l => (
+            <div className={`${style.item}`}>{l.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
 export default LogementsListe;
