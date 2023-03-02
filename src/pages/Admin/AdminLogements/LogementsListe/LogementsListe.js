2290807b93b50bb4eae4d35d27e6049a4ff11157
Corentin Desilles <corentindesilles974@gmail.com>
2023-03-02 10:43:54 +0100

    navigation edition et appelle fonction delete

--- src/pages/Admin/AdminLogements/LogementsListe/LogementsListe.js
+++ src/pages/Admin/AdminLogements/LogementsListe/LogementsListe.js
@@ -1,12 +1,24 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { useFetchLogements } from '../../../../hooks';
 import Loading from '../../../../components/Loading/Loading';
-import { ApiContext } from '../../../../context/ApiContext';
+// import { ApiContext } from '../../../../context/ApiContext';
 import style from './LogementsListe.module.scss';
+import { deleteLogement as deleteL } from '../../../../apis';
+import { NavLink } from 'react-router-dom';
 
 function LogementsListe() {
-  const BASE_URL_API = useContext(ApiContext);
-  const [logements, isLoading] = useFetchLogements(BASE_URL_API);
+  // const BASE_URL_API = useContext(ApiContext);
+  const [logements, setLogements, isLoading] = useFetchLogements();
+
+  async function deleteLogement(_id) {
+    console.log(_id);
+    await deleteL(_id);
+    setLogements(logements.filter(l => l._id !== _id));
+  }
+
+  // async function updateLogement(_id) {
+  //   redirect(`/admin/logements/edit/${_id}`);
+  // }
 
   return (
     <div>
@@ -17,10 +29,17 @@ function LogementsListe() {
       ) : (
         <div className={`${style.list}`}>
           {logements.map(l => (
-            <div className={`${style.item}`}>
+            <div key={l._id} className={`${style.item}`}>
               <span className="flex-fill">{l.title}</span>
-              <button className="btn btn-primary mr-15">Editer</button>
-              <button className="btn btn-danger">Supprimer</button>
+              <NavLink to={`/admin/logements/edit/${l._id}`}>
+                <button className="btn btn-primary mr-15"> Editer</button>
+              </NavLink>
+              <button
+                onClick={() => deleteLogement(l._id)}
+                className="btn btn-danger"
+              >
+                Supprimer
+              </button>
             </div>
           ))}
         </div>
